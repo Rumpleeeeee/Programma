e5a45d49c83989773c23ada82c6a7a50fe01c3af
Rumpleeeeee <1696645238@qq.com>
2017-06-26 16:05:34 +0800

    斐波那契数列

--- Offer66.cpp
+++ Offer66.cpp
@@ -133,3 +133,35 @@ private:
     stack<int> stack1;
     stack<int> stack2;
 };
+
+
+/*
+P6-斐波那契数列
+时间限制：1秒 空间限制：32768K 
+题目描述
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+n<=39
+*/
+class Solution {
+public:
+    int Fibonacci(int n) 
+    {
+        //if(n == 0)return 0;
+        //else if(n == 1)return 1;
+        //else return Fibonacci(n-1) + Fibonacci(n-2);
+        
+        int result[2] = {0, 1};
+        if(n < 2)return result[n];
+        int Plus1 = 0;
+        int Plus2 = 1;
+        int PlusN = 0;
+        for(int i = 2; i <= n; i++)
+        {
+            PlusN = Plus1 + Plus2;
+            Plus1 = Plus2;
+            Plus2 = PlusN;
+        }
+        return PlusN;
+    }
+};
+
